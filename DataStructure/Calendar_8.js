56916865a8c2a9b35ee84dea2918b12bab1bba37
ADMIN <sangita.awaghad1987@gmail.com>
2019-08-30 15:04:04 +0530

    Calendar Two D Array

--- DataStructure/Calendar_8.js
+++ DataStructure/Calendar_8.js
@@ -1,3 +1,12 @@
+/******************************************************************************************
+ * @purpose	:store the calendar in an 2D array,the first dimension the week of the month and 
+ * 			the second dimension stores the day.prints the calendar of the month. of the week
+ *
+ * @author 	:sangita avghad
+ * @version	:1.0
+ * @since	:20-08-2019
+ * 
+ ******************************************************************************************/
 const utility = require('../Utility/Utility');
 calender = () => {
     try {
@@ -9,11 +18,11 @@ calender = () => {
             throw "month should be between 1 and 12";
         if (day <= 0 || day > 31)
             throw "day should be between 1 and 31";
-        let calenderMonth=["","January","February","March","April","May","June","July","August","September","October","November","December"];
+        let calenderMonth = ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
         let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
         let monthDays = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-        if (utility.isLeapYear(year)) {
+        if (utility.isLeapYear(year)) {//if given year is leap year month contain 29 days
             monthDays[2] = 29;
         }
         let dayOfWeekIndex = utility.getDayOfWeek(month, day, year);
@@ -23,8 +32,8 @@ calender = () => {
             console.log(`\n\ngiven date :${day}-${month}-${year}`);
             console.log(`day of week :${days[dayOfWeekIndex["result"]]}`);
         }
-        let mDays = new Array(7);
-        let mMonth = [[], []];
+
+        let mMonth = [[], []];//two day storing week and 7 days
         let count = 0;
         let i = 0;
         mMonth[i] = new Array(7);
@@ -33,8 +42,8 @@ calender = () => {
             count++;
         }
         let d = 1;
-        
-        while (d <=monthDays[month]) {
+
+        while (d <= monthDays[month]) {
             mMonth[i][count] = d++;
             count++;
             if (count % 7 == 0) {
@@ -49,24 +58,21 @@ calender = () => {
         }
         console.log(`\n\t${calenderMonth[month]} ${year}`);
         console.log(`${days[0]} ${days[1]} ${days[2]} ${days[3]} ${days[4]} ${days[5]} ${days[6]}`);
-        for(let i=0;i<mMonth.length-1;i++)
-        {   mMonth[i];
-            for(let j=0;j<mMonth[i].length;j++)
-            {
-                if(mMonth[i][j]==0)
+        for (let i = 0; i < mMonth.length - 1; i++) {
+            mMonth[i];
+            for (let j = 0; j < mMonth[i].length; j++) {
+                if (mMonth[i][j] == 0)
                     process.stdout.write(`    `);
-                else if(mMonth[i][j]<10)
+                else if (mMonth[i][j] < 10)
                     process.stdout.write(`${mMonth[i][j]}   `);
                 else
                     process.stdout.write(`${mMonth[i][j]}  `);
             }
             console.log();
         }
-
-        
     } catch (e) {
         console.log(`error occured :${e}`);
-        
+
     }
 
 }
