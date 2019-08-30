302331c746a84feac1d9b1290f29f9c05cbbb246
ADMIN <sangita.awaghad1987@gmail.com>
2019-08-30 15:18:45 +0530

    implement month of calendar using stack using linked list

--- DataStructure/CalendarQueStack_10.js
+++ DataStructure/CalendarQueStack_10.js
@@ -1,3 +1,12 @@
+
+/******************************************************************************************
+ * @purpose	:Implement month of calendar using stack using linked list
+ * 
+ * @author 	:sangita avghad
+ * @version	:1.0
+ * @since	:30-08-2019
+ * 
+ ******************************************************************************************/
 const utility = require('../Utility/Utility');
 const linkList = require("../Utility/LinkedList");
 
@@ -6,7 +15,7 @@ calender = () => {
         let month = parseInt(process.argv[2]);
         let day = 1;
         let year = parseInt(process.argv[4]);
-        let listQueObj = new linkList.LinkList();
+        let listQueObj = new linkList.LinkList();//create linked list object
         if (month <= 0 || month > 12)
             throw "month should be between 1 and 12";
         if (day <= 0 || day > 31)
@@ -15,10 +24,10 @@ calender = () => {
         let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
         let monthDays = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-        if (utility.isLeapYear(year)) {
+        if (utility.isLeapYear(year)) {//check given year is leap year or not if yes set monthDays to 29
             monthDays[2] = 29;
         }
-        let dayOfWeekIndex = utility.getDayOfWeek(month, day, year);
+        let dayOfWeekIndex = utility.getDayOfWeek(month, day, year);//return index of day of week
 
         let mDays = new Array(7);
         let mMonth = [[], []];
@@ -28,8 +37,8 @@ calender = () => {
         mMonth[i] = new Array(7);
         while (count < dayOfWeekIndex["result"]) {
             mMonth[i][count] = 0;
-            let nodeObj1 = new linkList.DayNode(days[count], 0, null);
-            listQueObj.insertAtEnd(nodeObj1);
+            let nodeObj1 = new linkList.DayNode(days[count], 0, null);//create calender node object
+            listQueObj.insertAtEnd(nodeObj1);//call insert at end method of linked list to insert node
             count++;
 
         }
@@ -39,7 +48,7 @@ calender = () => {
 
             count++;
             let nodeObj2 = new linkList.DayNode(days[count], d++, null)
-            listQueObj.insertAtEnd(nodeObj2);
+            listQueObj.insertAtEnd(nodeObj2);//call insert at end method of linked list to insert node
             if (count % 7 == 0) {
                 i++;
                 mMonth[i] = new Array(7);
@@ -50,34 +59,34 @@ calender = () => {
             mMonth[i][count] = 0;
             count++;
             let nodeObj3 = new linkList.DayNode(days[count], 0, null)
-            listQueObj.insertAtEnd(nodeObj);
+            listQueObj.insertAtEnd(nodeObj);//call insert at end method of linked list to insert node in queue
         }
 
         console.log(`\n\t${calenderMonth[month]} ${year}`);
         console.log(`${days[0]} ${days[1]} ${days[2]} ${days[3]} ${days[4]} ${days[5]} ${days[6]}`);
 
         let listStkObj1 = new linkList.LinkList();
-        let temp = listQueObj.HEAD.next;
+        let temp = listQueObj.HEAD.next;//get head of linked list which is call as top i.e front of the queue
         while(temp!=null)
         {
             let nodeStkObj1 = new linkList.DayNode(days[count],temp.data, null);
-            listStkObj1.insertFirst(nodeStkObj1);
+            listStkObj1.insertFirst(nodeStkObj1);//call insert at first method of linked list to insert node in first stack
             temp=temp.next;
         }
 
         let listStkObj2 = new linkList.LinkList();
-        temp = listStkObj1.HEAD;
+        temp = listStkObj1.HEAD;//get head of linked list which is call as top i.e top of first stack
         while(temp!=null)
         {
             let nodeStkObj2 = new linkList.DayNode(days[count],temp.data, null);
-            listStkObj2.insertFirst(nodeStkObj2);
+            listStkObj2.insertFirst(nodeStkObj2);//call insert at first method of linked list to insert node in second stack
             temp=temp.next;
         }
 
         count = 0;
-        let top=listStkObj2.HEAD.next;
+        let top=listStkObj2.HEAD.next;//get head of linked list which is call as top i.e top of second stack
         while (top != null) {
-            let date = top.data;
+            let date = top.data;//get date from linekd list node
             if (date == 0)
                 process.stdout.write(`    `);
             else if (date < 10)
@@ -85,7 +94,7 @@ calender = () => {
             else
                 process.stdout.write(`${date}  `);
 
-            top = top.next;
+            top = top.next;//iterate head of linked list still last node
             count++;
             if (count % 7 == 0)
                 console.log();
